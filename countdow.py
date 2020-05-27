a9b4d0e708772845a41c4afc239d71f2da05397e
Zelmyx <kryss10an@gmail.com>
2020-05-27 12:53:10 +0200

    feat(recursive_permutations): finished basic funcionality of func recursive permutations

--- countdow.py
+++ countdow.py
@@ -1,11 +1,38 @@
 
 def solve(numbers, target):
-    pass
+    recursive_permutations(numbers, target, [])
 
 
 def recursive_permutations(numbers, target, expression):
-    pass
+    for i in range(len(numbers)):
+        expression.append(numbers.pop(i))
+
+        calculate(expression)
+
+        if len(numbers) == 0:
+            break
+
+        for i in range(4):
+            if i == 0:
+                expression.append("*")
+            elif i == 1:
+                expression.append("+")
+            elif i == 2:
+                expression.append("/")
+            elif i == 3:
+                expression.append("-")
+            recursive_permutations(numbers, target, expression)
+
+        numbers.insert(0, expression.pop())
+    if len(expression) % 2 == 1:
+        numbers.insert(0, expression.pop())
+    if len(expression) != 0:
+        expression.pop()
+    return
 
 
 def calculate(expression):
     pass
+
+
+solve([1, 2, 3], 6)
